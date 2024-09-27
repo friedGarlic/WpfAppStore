b561e895fc25888040a2b44065d36533bcd45d13
remcarlmerza <remcarlmerza@gmail.com>
2024-09-28 01:29:53 +0800

    Add project files.

---
+++ WpfApp/UserControls/XApp.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MiscUtil;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for XApp.xaml
+    /// </summary>
+    public partial class XApp : UserControl
+    {
+        public XApp()   
+        {
+            InitializeComponent();
+            List<string> filePaths = Directory.GetFiles(Environment.CurrentDirectory + @"\..\..\Images", "*.png").ToList<string>();
+
+            FileInfo randomFileInfo = new FileInfo(filePaths[StaticRandom.Next(filePaths.Count)]);
+
+            ProductImage.Source = new BitmapImage(new Uri(randomFileInfo.FullName, UriKind.RelativeOrAbsolute));
+
+            AppNameText.Text = (new CultureInfo("en-US", false).TextInfo).
+                ToTitleCase(
+                    randomFileInfo.FullName.Split('\\').Last().Split('-').Last().Split('.').First()
+                );
+
+            
+        }
+
+        private void ProductImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
